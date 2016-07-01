286ced76720d4dbcedfea6b67645ae02efe0b1cb
David Diaz <david.dia.isei@gmail.com>
2016-07-01 15:52:55 +0200

    wip

--- test/agenda-pg.js
+++ test/agenda-pg.js
@@ -1,6 +1,25 @@
 
-describe('Agenda-pg', function() {
-  it('stuff', function() {
-    
+
+var expect = require('expect.js'),
+    path = require('path'),
+    moment = require('moment-timezone'),
+    cp = require('child_process'),
+    Agenda = require( path.join('..', 'index.js') ),
+    Job = require( path.join('..', 'lib', 'job.js') );
+
+describe('agenda-pg', function() {
+  var agenda;
+  beforeEach(function(done) {
+    var agenda = new Agenda({
+      pg: {
+        user: 'postgresql',
+        database: 'test',
+        port: 5432,
+      }
+    }, done);
+  });
+
+  it.only('stuff', function() {
+
   });
 });
